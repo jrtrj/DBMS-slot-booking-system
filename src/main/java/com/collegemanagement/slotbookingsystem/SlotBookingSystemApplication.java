ca4c2ab5aef09719fbe6034f6a6006d790d45a7a
jrtrj <jerit.reji.dev@gmail.com>
2025-10-19 09:36:50 -0400

    first commit

---
+++ src/main/java/com/collegemanagement/slotbookingsystem/SlotBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.collegemanagement.slotbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegemanagement/slotbookingsystem/SlotBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegemanagement.slotbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
