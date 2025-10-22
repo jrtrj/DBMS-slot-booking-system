bd28b2f6bfe23b2bfcd9b8ed4cfbbaa0f9b68b5e
jrtrj <jerit.reji.dev@gmail.com>
2025-10-22 12:20:54 -0400

    fixes

---
+++ src/main/java/com/collegemanagement/slotbookingsystem/services/VenueService.java
@@ -0,0 +1,21 @@
+package com.collegemanagement.slotbookingsystem.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.collegemanagement.slotbookingsystem.dao.venue.VenueDao;
+import com.collegemanagement.slotbookingsystem.model.Venue;
+
+@Service
+public class VenueService {
+	private final VenueDao venueDao;
+
+	public VenueService(VenueDao venueDao) {
+		this.venueDao = venueDao;
+	}
+	
+	public List<Venue> getAllVenues() {
+		return venueDao.findAll();
+	}
+}
