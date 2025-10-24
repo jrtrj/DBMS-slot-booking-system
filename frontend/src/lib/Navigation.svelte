9e9d8fc9836cf0480597ac84a61557ea2e6d5599
JoThePOkeMOn <joelmathewsamuel@gmail.com>
2025-10-25 01:37:09 +0530

    add event card ui

---
+++ frontend/src/lib/Navigation.svelte
@@ -0,0 +1,8 @@
+<script></script>
+<main>
+    <div class="nav">
+        <a href="/home">
+            <img src="/house-regular.svg" alt="">
+        </a>
+    </div>
+</main>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Test from "$lib/Test.svelte";
+    import EventCard from "$lib/EventCard.svelte";
+	import Navigation from "$lib/Navigation.svelte";
+</script>
+<main> 
+    <EventCard />
+    <Navigation />
+</main>
